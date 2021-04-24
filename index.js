de8f8a44b21ebba12f86f5eb40ab778fbb56ce36
Adam Harnusek <adam.harnusek@gmail.com>
2021-04-24 10:31:47 +0200

    Testing REST API from pod

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 var express = require('express')
 var app = express()
-// const axios = require('axios')
+const axios = require('axios')
 
 app.use(express.static('public'))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
 
 let animals = [
@@ -41,20 +41,31 @@ let animals = [
  *     description: Greeting message.
  */
 app.get('/', function (req, res) {
-   res.json({"message" : "Hello GET / route"});
+    res.json({ "message": "Hello GET / route" });
 })
 
+
+app.get("/api", async (req, res) => {
+    const token = "sha256~ZyYAmTZg9-WUdjKowjMxvrN1-ZrS2B_59uKt9JxEQ9Y"
+    const res = await axios.get("https://api.crc.testing:6443/api/v1/namespaces/monitoring-cluster/pods", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    res.json(res.data)
+})
 /*
  * @api [get] /animals
- * description: Call another pod in Openshift cluster(like middleware)
+ * description: Return array of animals
  * responses:
  *   200:
  *     description: Response from pod.
  */
 app.get('/animals', async function (req, res) {
-   res.json(animals)
+    res.json(animals)
 })
 
-app.listen(8080, function () {
-   console.log("Example app listening at port 8080")
+const port = process.env.PORT || 8081
+app.listen(port, function () {
+    console.log("Example app listening at port " + port)
 })
