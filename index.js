8e1dd10aada443e9aa40d461571ebe64ad57d689
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-01 15:14:39 +0200

    Create websocket route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 var app = express()
 const axios = require('axios')
 const https = require('https')
+import * as k8s from '@kubernetes/client-node';
 
 app.use(express.static('public'))
 app.use(function (req, res, next) {
@@ -12,6 +13,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const agent = https.Agent({
@@ -52,6 +54,48 @@ let animals = [
  */
 app.get('/', function (req, res) {
     res.json({ "message": "Hello GET / route" });
+})
+
+app.get("/socket", async (req, res) => {
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+
+    const watch = new k8s.Watch(kc);
+    watch.watch('/api/v1/namespaces',
+        // optional query parameters can go here.
+        {
+            allowWatchBookmarks: true,
+        },
+        // callback is called for each received object.
+        (type, apiObj, watchObj) => {
+            if (type === 'ADDED') {
+                // tslint:disable-next-line:no-console
+                console.log('new object:')
+            } else if (type === 'MODIFIED') {
+                // tslint:disable-next-line:no-console
+                console.log('changed object:')
+            } else if (type === 'DELETED') {
+                // tslint:disable-next-line:no-console
+                console.log('deleted object:')
+            } else if (type === 'BOOKMARK') {
+                // tslint:disable-next-line:no-console
+                console.log(`bookmark: ${watchObj.metadata.resourceVersion}`)
+            } else {
+                // tslint:disable-next-line:no-console
+                console.log('unknown type: ' + type)
+            }
+            // tslint:disable-next-line:no-console
+            console.log(apiObj)
+        },
+        // done callback is called if the watch terminates normally
+        (err) => {
+            // tslint:disable-next-line:no-console
+            console.log(err)
+        })
+        .then((req) => {
+            // watch returns a request object which you can use to abort the watch.
+            setTimeout(() => { req.abort(); }, 10 * 1000)
+        })
 })
 
 
